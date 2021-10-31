13ee8797de71a7c8344102cce9d125229c9a6f54
Yevhen Baidiuk <y.baidiuk@gmail.com>
2021-10-31 14:02:51 +0100

    left side and top finish

--- vis.js
+++ vis.js
@@ -1,8 +1,8 @@
 const width = 960
 const height = 500
-const margin = 200
-const padding = 200
-const adj = 30
+const margin = 300
+const padding = 100
+const adj = 100
 // we are appending SVG first
 const svg = d3.select('div#container').append('svg')
     .attr('preserveAspectRatio', 'xMinYMin meet')
@@ -19,38 +19,39 @@ const svg = d3.select('div#container').append('svg')
 const dataset = d3.csv('usa_nominal_gdp_1997-2020.csv')
 dataset.then(data => {
     console.log(data)
-    let slices = data.map(obj=> { //remove last one. (columns names)
+    let slices = data.map(obj => { //remove last one. (columns names)
 
         return {
             id: obj.State,
-            values: Object.keys(obj).filter(x=>x!=='State').map(key=>{
+            values: Object.keys(obj).filter(x => x !== 'State').map(key => {
                 return {
                     date: key,
                     measurement: +obj[key]
-                };
+                }
             })
-        };
-    });
+        }
+    })
 
     console.log('slices, ', slices)
 
 //----------------------------SCALES----------------------------//
     const yearsArr = data.columns.slice(1)
-    const xScale = d3.scaleLinear().range([0,width]);
-    const yScale = d3.scaleLinear().range([height, 0]);
-    xScale.domain(d3.extent(yearsArr, d=>d));
+    const xScale = d3.scaleLinear().range([0, width])
+    const yScale = d3.scaleLinear().range([height, 0])
+    xScale.domain(d3.extent(yearsArr, d => d))
     yScale.domain([(0), d3.max(slices, c => d3.max(c.values, d => d.measurement + 4))
-    ]);
+    ])
 
 //-----------------------------AXES-----------------------------//
     const yaxis = d3.axisLeft()
+        .tickFormat(n => n / 1000000 + ' M')
         .ticks((slices[0].values).length)
-        .scale(yScale);
+        .scale(yScale)
 
     const xaxis = d3.axisBottom()
         .tickFormat(d3.format('d'))
         .ticks(yearsArr.length)
-        .scale(xScale);
+        .scale(xScale)
 
 //----------------------------LINES-----------------------------//
     const line = d3.line()
@@ -59,40 +60,42 @@ dataset.then(data => {
 
 //-------------------------2. DRAWING---------------------------//
 //-----------------------------AXES-----------------------------//
-    svg.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xaxis);
+    svg.append('g')
+        .attr('class', 'axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xaxis)
 
-    svg.append("g")
-        .attr("class", "axis")
+    svg.append('g')
+        .attr('class', 'axis')
         .call(yaxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("dy", ".75em")
-        .attr("y", 6)
-        .style("text-anchor", "end")
-        .text("Frequency");
+        .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('dy', '-5em')
+        .attr('dx', '-10em')
+        .style('text-anchor', 'end')
+        .text('Nominal GDP(Millions of current dollars)')
 
 //----------------------------LINES-----------------------------//
-    const lines = svg.selectAll("lines")
+    const lines = svg.selectAll('lines')
         .data(slices)
         .enter()
-        .append("g");
+        .append('g')
 
-    lines.append("path")
-        .attr("class", "line-0")
-        .attr("d", d=>  line(d.values));
+    lines.append('path')
+        .attr('class', 'line-0')
+        .attr('d', d => line(d.values))
 
-    lines.append("text")
-        .attr("class","serie_label")
-        .datum(d=> {
+    lines.append('text')
+        .attr('class', 'serie_label')
+        .datum(d => {
             return {
                 id: d.id,
-                value: d.values[d.values.length - 1]}; })
-        .attr("transform", d=> "translate(" + (xScale(d.value.date) + 10)
-                + "," + (yScale(d.value.measurement) + 5 ) + ")")
-        .attr("x", 5)
-        .text(d=>  ("Serie ") + d.id);
+                value: d.values[d.values.length - 1]
+            }
+        })
+        .attr('transform', d => 'translate(' + (xScale(d.value.date) + 10)
+            + ',' + (yScale(d.value.measurement) + 5) + ')')
+        .attr('x', 5)
+        .text(d => d.id)
 
 });
